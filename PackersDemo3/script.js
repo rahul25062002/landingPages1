f6cf63d43764e8a8a363e21b90a6031db6081bec
rahul25062002 <mmgbrahul@gmail.com>
2025-01-01 00:20:11 +0530

    second commit

---
+++ PackersDemo3/script.js
@@ -0,0 +1,32 @@
+// Assuming you're using a library like Slick:
+$(document).ready(function(){
+    $('.hero-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
+
+  // Assuming you're using a library like Slick:
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
+  $(document).ready(function(){
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
